6bec2795b293dc0432e8f5b0c5422301c9996092
Birgitta <birgitta.oberg@metropolia.fi>
2024-03-01 10:24:35 +0200

    fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,25 +1,22 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv'); // Make sure to install dotenv if you haven't already
-dotenv.config(); // Load environment variables from a .env file
+const dotenv = require('dotenv');
+dotenv.config();
 
 const connectDB = async () => {
   try {
     const connection = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true,
     });
 
     console.log(`MongoDB Connected: ${connection.connection.host}`);
 
-    // Include your models here
-    // For example:
-    require('./models/userModel');
-    require('./models/goalModel');
+    require('../models/userModel');
+    require('../models/goalModel');
 
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
-    process.exit(1); // Exit process with failure
+    process.exit(1);
   }
 };
 
