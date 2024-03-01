3079312fd2921bfe75682437a2f30c0771203272
Birgitta <birgitta.oberg@metropolia.fi>
2024-03-01 10:34:30 +0200

    fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
+mongoose.set('strictQuery', false);
+
 const connectDB = async () => {
   try {
     const connection = await mongoose.connect(process.env.MONGO_URI, {
